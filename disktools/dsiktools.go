ed0969998db4f3d88e8f00f0e682c1869dcc607c
OnlyPiglet <jackwuchenghao@163.com>
2023-11-22 15:06:26 +0800

    :sparkles: add disk size switch

--- disktools/dsiktools.go
+++ disktools/dsiktools.go
@@ -7,10 +7,10 @@ import (
 
 const (
 	_   = iota
-	kiB = 1 << (10 * iota)
-	miB
-	giB
-	tiB
+	KiB = 1 << (10 * iota)
+	MiB
+	GiB
+	TiB
 )
 
 // FormatDiskSizeBytes 将字节转换为mib/kib/gib/tib , 入参大小格式为 int64
@@ -19,17 +19,17 @@ func FormatDiskSizeBytes(bytes int64) string {
 	unit := ""
 
 	switch {
-	case bytes >= tiB:
-		output = output / tiB
+	case bytes >= TiB:
+		output = output / TiB
 		unit = "Ti"
-	case bytes >= giB:
-		output = output / giB
+	case bytes >= GiB:
+		output = output / GiB
 		unit = "Gi"
-	case bytes >= miB:
-		output = output / miB
+	case bytes >= MiB:
+		output = output / MiB
 		unit = "Mi"
-	case bytes >= kiB:
-		output = output / kiB
+	case bytes >= KiB:
+		output = output / KiB
 		unit = "Ki"
 	case bytes == 0:
 		return "0"
@@ -38,4 +38,28 @@ func FormatDiskSizeBytes(bytes int64) string {
 	result := strconv.FormatFloat(output, 'f', 0, 64)
 	result = strings.TrimSuffix(result, ".0")
 	return result + unit
+}
+
+func BytesToKiB(bytes int64) int64 {
+	output := float64(bytes)
+	output = output / KiB
+	return int64(output)
+}
+
+func BytesToGiB(bytes int64) int64 {
+	output := float64(bytes)
+	output = output / GiB
+	return int64(output)
+}
+
+func BytesToMiB(bytes int64) int64 {
+	output := float64(bytes)
+	output = output / MiB
+	return int64(output)
+}
+
+func BytesToTiB(bytes int64) int64 {
+	output := float64(bytes)
+	output = output / TiB
+	return int64(output)
 }
