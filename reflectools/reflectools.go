f0207ec2837092104554c23f08d6deed4c7911ef
OnlyPiglet <jackwuchenghao@163.com>
2023-12-14 16:14:22 +0800

    :docs: add some thanks link

--- reflectools/reflectools.go
+++ reflectools/reflectools.go
@@ -2,6 +2,8 @@ package reflectools
 
 import "reflect"
 
+//reflect thanks for https://www.cnblogs.com/jiujuan/p/17142703.html
+
 func GetInterfaceType(a any) string {
 	return reflect.TypeOf(a).String()
 }
