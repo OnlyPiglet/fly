cd57a54ce041b1ac6a0d4b4c1f7248f56e518850
OnlyPiglet <jackwuchenghao4@gmail.com>
2024-07-29 19:28:59 +0800

    feat: change code block

---
+++ httptools/reuse_reader.go
@@ -0,0 +1,36 @@
+package httptools
+
+import (
+	"bytes"
+	"io"
+)
+
+type reusableReader struct {
+	io.Reader
+	readBuf *bytes.Buffer
+	backBuf *bytes.Buffer
+}
+
+func ReusableReader(r io.Reader) io.Reader {
+	readBuf := bytes.Buffer{}
+	readBuf.ReadFrom(r) // error handling ignored for brevity
+	backBuf := bytes.Buffer{}
+
+	return reusableReader{
+		io.TeeReader(&readBuf, &backBuf),
+		&readBuf,
+		&backBuf,
+	}
+}
+
+func (r reusableReader) Read(p []byte) (int, error) {
+	n, err := r.Reader.Read(p)
+	if err == io.EOF {
+		r.reset()
+	}
+	return n, err
+}
+
+func (r reusableReader) reset() {
+	io.Copy(r.readBuf, r.backBuf) // nolint: errcheck
+}
