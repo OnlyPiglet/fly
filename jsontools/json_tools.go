0307fe5424ce9de414f207167b540fe65557912c
OnlyPiglet <jackwuchenghao4@gmail.com>
2024-10-03 11:29:32 +0800

    add json mash

--- jsontools/json_tools.go
+++ jsontools/json_tools.go
@@ -1,6 +1,7 @@
 package jsontools
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/OnlyPiglet/fly"
 )
@@ -16,4 +17,12 @@ func JsonMash[T any](obj T) (string, error) {
 func JsonUnMash[T any](objJson string, t T) error {
 	objBytes := fly.QuickStringToBytes(objJson)
 	return json.Unmarshal(objBytes, t)
+}
+
+func JsonMashWithoutEscapeHTML(t interface{}) (string, error) {
+	bf := bytes.NewBuffer([]byte{})
+	jsonEncoder := json.NewEncoder(bf)
+	jsonEncoder.SetEscapeHTML(false)
+	err := jsonEncoder.Encode(t)
+	return bf.String(), err
 }
