2f2c8cb174b3133d2a4a4924859b51c359225c4b
OnlyPiglet <jackwuchenghao4@gmail.com>
2024-11-10 17:22:21 +0800

    add xff

---
+++ stringtools/stringtools.go
@@ -0,0 +1,13 @@
+package stringtools
+
+import "math/rand"
+
+func RandomString(n int) string {
+	var letterRunes = []rune("-%*^#@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
