85e024a4d2f015b81e0b649bb0e9338868fa66ab
OnlyPiglet <jackwuchenghao4@gmail.com>
2024-12-29 14:32:56 +0800

    fix: zero int omit

--- stringtools/stringtools.go
+++ stringtools/stringtools.go
@@ -1,6 +1,10 @@
 package stringtools
 
-import "math/rand"
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
 
 func RandomString(n int) string {
 	var letterRunes = []rune("-%*^#@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -20,4 +24,14 @@ func RandomNormalString(n int) string {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return string(b)
+}
+
+func RandomTimeNormalString(n int) string {
+	var letterRunes = []rune("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b) + "_" + strconv.FormatInt(time.Now().Unix(), 10)
 }
