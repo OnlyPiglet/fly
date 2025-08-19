404f83b3d95480204d8f9c0464d6e6a058be96cd
OnlyPiglet <jackwuchenghao4@gmail.com>
2025-08-19 14:31:36 +0800

    add iface ipv4 get func

--- servertools/servertools_test.go
+++ servertools/servertools_test.go
@@ -9,3 +9,11 @@ func TestServerInfo(t *testing.T) {
 	in := ServerInfo()
 	println(fmt.Sprintf("%+v", in))
 }
+
+func TestGetIPv4ByInterfaceName(t *testing.T) {
+	iface, err := GetIPv4ByInterfaceName("en0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	println(fmt.Sprintf("%+v", iface))
+}
