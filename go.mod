e1f6b48ea6c9ffc9d0e6af0ca4912f0efd8a0124
OnlyPiglet <jackwuchenghao4@gmail.com>
2025-08-20 10:26:33 +0800

    add iface ipv4 get func

--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ toolchain go1.24.5
 
 require (
 	github.com/disintegration/imaging v1.6.2
+	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/shirou/gopsutil/v3 v3.23.10
 )
 
 require (
+	github.com/BurntSushi/toml v1.5.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
@@ -20,4 +22,6 @@ require (
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 	golang.org/x/sys v0.34.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- loggertools/writer.go
+++ loggertools/writer.go
@@ -1,69 +1,12 @@
 package logger
 
 import (
-	"bufio"
 	"bytes"
 	"io"
 	"net"
-	"os"
-	"path/filepath"
 	"sync"
-	"sync/atomic"
 	"time"
 )
-
-type FileWriter struct {
-	f       *os.File
-	rotator Rotator
-	path    string
-	wSize   *atomic.Int64
-	bw      *bufio.Writer
-	flushAt time.Time
-}
-
-func (f *FileWriter) open() {
-	f.f, _ = os.OpenFile(f.path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	f.bw = bufio.NewWriter(f.f)
-}
-
-func (f *FileWriter) loadSize() {
-	fs, _ := f.f.Stat()
-	f.wSize.Store(fs.Size())
-}
-
-func (f *FileWriter) Close() error {
-	_ = f.bw.Flush()
-	return f.f.Close()
-}
-
-func (f *FileWriter) flush() {
-	if time.Now().Add(-time.Second).After(f.flushAt) && f.bw.Buffered() > 0 {
-		_ = f.bw.Flush()
-		f.flushAt = time.Now()
-	}
-}
-
-func (f *FileWriter) Write(p []byte) (int, error) {
-	n, err := f.bw.Write(p)
-	f.wSize.Add(int64(n))
-	if f.wSize.Load() >= f.rotator.MaxSize() {
-		_ = f.bw.Flush()
-		_ = f.f.Close()
-		f.rotator.Rotate(f.path)
-		f.open()
-		f.wSize.Store(0)
-	}
-	f.flush()
-	return n, err
-}
-
-func NewFileWriter(path string, rotator Rotator) io.WriteCloser {
-	_ = os.MkdirAll(filepath.Dir(path), 0755)
-	fw := &FileWriter{path: path, rotator: rotator, wSize: new(atomic.Int64), flushAt: time.Now()}
-	fw.open()
-	fw.loadSize()
-	return fw
-}
 
 type NetworkWriter struct {
 	conn       net.Conn
