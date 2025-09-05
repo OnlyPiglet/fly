7245ba1ef40b21c884f67db3cff6350a31955336
OnlyPiglet <jackwuchenghao4@gmail.com>
2025-09-05 16:41:00 +0800

    添加功能

--- go.mod
+++ go.mod
@@ -9,19 +9,26 @@ require (
 	github.com/IBM/sarama v1.46.0
 	github.com/disintegration/imaging v1.6.2
 	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/redis/go-redis/extra/redisotel/v9 v9.13.0
+	github.com/redis/go-redis/v9 v9.13.0
 	github.com/shirou/gopsutil/v3 v3.23.10
+	go.opentelemetry.io/otel/sdk v1.37.0
 )
 
 require (
 	github.com/BurntSushi/toml v1.5.0 // indirect
 	github.com/ClickHouse/ch-go v0.67.0 // indirect
 	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/eapache/go-resiliency v1.7.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
 	github.com/eapache/queue v1.1.0 // indirect
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.7.1 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -37,6 +44,7 @@ require (
 	github.com/pierrec/lz4/v4 v4.1.22 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20250401214520-65e299d6c5c9 // indirect
+	github.com/redis/go-redis/extra/rediscmd/v9 v9.13.0 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
@@ -44,7 +52,9 @@ require (
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/otel v1.37.0 // indirect
+	go.opentelemetry.io/otel/metric v1.37.0 // indirect
 	go.opentelemetry.io/otel/trace v1.37.0 // indirect
 	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
---
+++ redistools/option.go
@@ -0,0 +1,109 @@
+package redistools
+
+import (
+	"crypto/tls"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"go.opentelemetry.io/otel/sdk/trace"
+)
+
+// Option set the redis options.
+type Option func(*options)
+
+type options struct {
+	dialTimeout  time.Duration
+	readTimeout  time.Duration
+	writeTimeout time.Duration
+	tlsConfig    *tls.Config
+
+	// Note: this field is only used for Init and InitSingle, and the other parameters will be ignored.
+	singleOptions *redis.Options
+
+	// Note: this field is only used for InitSentinel, and the other parameters will be ignored.
+	sentinelOptions *redis.FailoverOptions
+
+	// Note: this field is only used for InitCluster, and the other parameters will be ignored.
+	clusterOptions *redis.ClusterOptions
+
+	// deprecated: use tp instead
+	enableTrace    bool
+	tracerProvider *trace.TracerProvider
+}
+
+func (o *options) apply(opts ...Option) {
+	for _, opt := range opts {
+		opt(o)
+	}
+}
+
+// default settings
+func defaultOptions() *options {
+	return &options{
+		enableTrace: false, // whether to enable trace, default off
+	}
+}
+
+// WithEnableTrace use trace, redis v8
+// Deprecated: use WithEnableTracer instead
+func WithEnableTrace() Option {
+	return func(o *options) {
+		o.enableTrace = true
+	}
+}
+
+// WithTracing set redis tracer provider, redis v9
+func WithTracing(tp *trace.TracerProvider) Option {
+	return func(o *options) {
+		o.tracerProvider = tp
+	}
+}
+
+// WithDialTimeout set dail timeout
+func WithDialTimeout(t time.Duration) Option {
+	return func(o *options) {
+		o.dialTimeout = t
+	}
+}
+
+// WithReadTimeout set read timeout
+func WithReadTimeout(t time.Duration) Option {
+	return func(o *options) {
+		o.readTimeout = t
+	}
+}
+
+// WithWriteTimeout set write timeout
+func WithWriteTimeout(t time.Duration) Option {
+	return func(o *options) {
+		o.writeTimeout = t
+	}
+}
+
+// WithTLSConfig set TLS config
+func WithTLSConfig(c *tls.Config) Option {
+	return func(o *options) {
+		o.tlsConfig = c
+	}
+}
+
+// WithSingleOptions set single redis options
+func WithSingleOptions(opt *redis.Options) Option {
+	return func(o *options) {
+		o.singleOptions = opt
+	}
+}
+
+// WithSentinelOptions set redis sentinel options
+func WithSentinelOptions(opt *redis.FailoverOptions) Option {
+	return func(o *options) {
+		o.sentinelOptions = opt
+	}
+}
+
+// WithClusterOptions set redis cluster options
+func WithClusterOptions(opt *redis.ClusterOptions) Option {
+	return func(o *options) {
+		o.clusterOptions = opt
+	}
+}
