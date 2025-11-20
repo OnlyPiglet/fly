f5f938df32114a8a20062a8268d6e7af62db846b
OnlyPiglet <jackwuchenghao4@gmail.com>
2025-11-20 13:51:04 +0800

    feat: add integration accurate rpc call and all notify

--- queuetools/queue_tools.go
+++ queuetools/queue_tools.go
@@ -1,4 +1,18 @@
 package queue
 
-type Queue interface {
+import "fmt"
+
+type Queue[V any] interface {
+	Enqueue(v []V) error
+	Dequeue() (V, error)
+	Len() (int64, error)
+}
+
+type QueueFullError struct {
+	name string
+	size int64
+}
+
+func (e *QueueFullError) Error() string {
+	return fmt.Sprintf("fly queue %s full size is %d ", e.name, e.size)
 }
