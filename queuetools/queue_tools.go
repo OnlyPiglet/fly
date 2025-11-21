9eb141ec67aba5c532683b1943254fc722249262
OnlyPiglet <jackwuchenghao4@gmail.com>
2025-11-21 17:24:14 +0800

    reids queue pop

--- queuetools/queue_tools.go
+++ queuetools/queue_tools.go
@@ -3,8 +3,11 @@ package queue
 import "fmt"
 
 type Queue[V any] interface {
+	//Enqueue []V 需要注意的数量过多的话，会导致内存过多 建议一次5000～10000条元素推送，不宜过多
 	Enqueue(v []V) error
 	Dequeue() (V, error)
+	//DequeueBatch 建议批量pop出数据，到本地处理，不然一个一个 pop 性能损耗过大
+	DequeueBatch(count int) ([]V, error)
 	Len() (int64, error)
 }
 
